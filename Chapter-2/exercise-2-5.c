6cb7f10db5db0abcb4cb9b94e3d6c0350df5042b
Ryan Linnit <ryan@ryanl.co.uk>
2016-12-13 22:43:56 +0000

    exercise-2-5.c

---
+++ Chapter-2/exercise-2-5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int any(char s1[], char s2[]);
+
+int main() {
+	int i;
+
+	i = any("ryan", "ai");
+
+	printf("%d\n", i);
+	
+}
+
+int any(char s1[], char s2[]) {
+	int i, k;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		for (k = 0; s2[k] != '\0'; k++)
+			if (s1[i] == s2[k])
+				return i;
+	
+	return -1;
+}
